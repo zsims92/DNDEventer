c781f01dad75552473e75cd12bc8026a11d39de3
Zach Sims <zachsims92@gmail.com>
2019-04-18 00:31:11 -0600

    Testing branches working with merges and branches

--- src/com/DNDeventer/mapEditor/editorProgram/EditorMain.java
+++ src/com/DNDeventer/mapEditor/editorProgram/EditorMain.java
@@ -4,6 +4,6 @@ import com.DNDeventer.mapEditor.MAPSTATS;
 
 public class EditorMain implements MAPSTATS {
     public static void run(){
-        System.out.println("Welcome to the map editor!!!.  Brando is a bitch tho.  He really is");
+        System.out.println("Welcome to the map editor!!!.  Brando is a bitch tho.  He really is so");
     }
 }
